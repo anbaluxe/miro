04de5fcbc46cba301d42d5439ac6f0add55aa96a
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-02-05 03:02:43 +0300

    config

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,5 +1,7 @@
+import { CONFIG } from "@/shared/model/config";
+
 function BoardsListPage() {
-  return <div>Boards list</div>;
+  return <div>Boards list {CONFIG.API_BASE_URL}</div>;
 }
 
 export const Component = BoardsListPage;
